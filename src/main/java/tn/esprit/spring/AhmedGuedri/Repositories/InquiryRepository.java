7e49e6a03cca30e7991a95556af8e4a47ddc0302
rahal <ahmed.rahal@esprit.tn>
2023-02-16 00:58:26 +0100

    Push test

--- src/main/java/tn/esprit/spring/AhmedGuedri/Repositories/InquiryRepository.java
+++ src/main/java/tn/esprit/spring/AhmedGuedri/Repositories/InquiryRepository.java
@@ -1,6 +1,7 @@
 package tn.esprit.spring.AhmedGuedri.Repositories;
 import org.springframework.data.repository.CrudRepository;
 import tn.esprit.spring.AhmedGuedri.entities.Inquiry;
+//aa
 public interface InquiryRepository extends CrudRepository<Inquiry, Long> {
 }
 
