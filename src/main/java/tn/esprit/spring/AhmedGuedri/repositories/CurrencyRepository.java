d5698858df4f0f2c80eca0acf20f8aa4a1d5b00a
Misow3002 <midou1928@live.fr>
2023-02-16 00:47:59 +0100

    Delete src/main/java/tn/esprit/spring/AhmedGuedri/repositories directory

--- src/main/java/tn/esprit/spring/AhmedGuedri/repositories/CurrencyRepository.java
+++
@@ -1,8 +0,0 @@
-//create Currency Repository   
-package tn.esprit.spring.AhmedGuedri.Repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import tn.esprit.spring.AhmedGuedri.entities.Currency;  
-
-public interface CurrencyRepository extends CrudRepository<Currency, Long> {
-}
